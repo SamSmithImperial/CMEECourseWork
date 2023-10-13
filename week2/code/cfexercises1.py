53d2b9cf18b52ff6afed0dcf1ed32bfdd51b52d3
Sam Smith <scs23@imperial.ac.uk>
2023-10-13 15:13:53 +0100

    adding cfexercises1.py

--- week2/code/cfexercises1.py
+++ week2/code/cfexercises1.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python3 
+
+# this shebang tells the computer where to look for python
+
+"""This exercise is to make this script a module using control_flow.py as an example""" # these triple quotation marks indicate a docstring which are part of the running code but are just used to describe the programme
+
+__appname__ = '[application name here]'
+__author__ = 'Your Name (your@email.address)'
+__version__ = '0.0.1'
+__license__ = "License for this code/program"
+
+# imports
+
+import sys
 
 def foo_1(x):
-    return x ** 0.5
+    return x ** 0.5 # ** is an exponentiation operator
 
 def foo_2(x, y):
     if x > y:
         return x
-    return y
+    return foo_2(5, 10)
 
 def foo_3(x, y, z):
     if x > y:
@@ -35,3 +49,11 @@ def foo_6(x): # Calculate the factorial of x in a different way; no if statement
         facto = facto * x
         x = x - 1
     return facto
+
+def main(argv):
+    print(foo_1(5))
+
+if __name__ == "__main__": 
+    """Makes sure the "main" function is called from command line"""  
+    status = main(sys.argv)
+    sys.exit(status)
