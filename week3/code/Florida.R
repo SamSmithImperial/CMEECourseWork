40b16fdd0c1a1f5821ae58d933ea68b21b27cfc6
Sam Smith <scs23@imperial.ac.uk>
2023-10-28 11:43:13 +0100

    Florida Prac Push

--- week3/code/Florida.R
+++ week3/code/Florida.R
@@ -4,6 +4,7 @@ set.seed(1)
 n <- length(ats$Year)
 Perm <- 100000
 variable <- ats$Temp
+TrueCorrelation <- cor(ats$Year, ats$Temp)
 
 # Generate permutations
 PermSamples <- replicate(Perm, sample(variable, size = n, replace = FALSE))
@@ -15,8 +16,8 @@ print(range(CorrelationCoefficients))
 
 # Plot the histogram
 hist(CorrelationCoefficients, xlim = c(-0.4, 0.6))
-abline(v = 0.53, col = "red", lwd = 3)
-text(x = 0.5, y = 15000, '0.53')
+abline(v = TrueCorrelation, col = "red", lwd = 3)
 
-
-
+Coef_more_than_realCor <- CorrelationCoefficients>TrueCorrelation
+count_true <- sum(Coef_more_than_realCor == TRUE)
+print(count_true/Perm)
