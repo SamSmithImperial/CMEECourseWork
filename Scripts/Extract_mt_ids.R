2f75b82414ca22aaae6c1290baa1edfdefdc8577
SamSmithImperial <146941256+SamSmithImperial@users.noreply.github.com>
2024-07-22 14:20:26 +0100

    Delete Scripts directory

--- Scripts/Extract_mt_ids.R
+++
@@ -1,9 +0,0 @@
-data <- read.csv("../data/SITE-100 Database - mitogenomes.csv", header = TRUE)
-
-data_with_family <- data[!is.na(data$family) & data$family != "" & data$order == "Coleoptera", ]
-
-mt_ids <- unique(data_with_family$mt_id)
-
-filepath <- "../data/mt_ids.txt"
-writeLines(mt_ids, filepath)
-
--- Scripts/Run_Project/make_sample_MGs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Source and destination directories
-source_dir="../data/Complete_MGs"
-dest_dir="../data/Sample_MGs"
-
-# List the files in the source directory, limit to the first 500 files
-ls -1 "$source_dir" | head -n 2000 | xargs -I {} cp "$source_dir/{}" "$dest_dir/"
